971db65a09ea45191b2c62345323614a137c9fbe
CATSUPERBERG <mixer468@gmail.com>
2021-10-15 16:48:53 +0300

    Settings menu calls SettingsService correctly (for sliders)

--- Assets/Prefabs/UI/SettingsMenu.cs
+++ Assets/Prefabs/UI/SettingsMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,14 @@ namespace GameSettings
     public class SettingsMenu : MonoBehaviour
     {
         private ISettingsService _settingsService;
-        private AudioVisualOptions _optionToSet;
+        private AudioVisualOptions _optionToSet = (AudioVisualOptions)(-1);
+        private int _optionsRegistered = 0;
         
         [Inject]
         public void Construct(ISettingsService settingsServiceImplementation)
         {
              if(settingsServiceImplementation == null)
-                throw new System.Exception("IGameFolders isn't provided to Settings");
+                throw new System.Exception("ISettingsService isn't provided to SettingsMenu");
                 
             _settingsService = settingsServiceImplementation;
         }
@@ -22,12 +24,64 @@ namespace GameSettings
         public void SetOptionToChange(InspectorOptionSelector optionContext)
         {
             _optionToSet = optionContext.OptionToSet;
-            // _settingsService.SetOption<float>(AudioVisualOptions.MasterVolume, value);
+            _optionsRegistered++;
         }
         
-        public void SetOptionValue(float value)
+        public void SetIntValue(int value)
         {
-            _settingsService.SetOption<float>(_optionToSet, value);            
+            if(validToExecute(value.GetType(), optionsHelper.OptionType(_optionToSet)))
+                _settingsService.SetOption<int>(_optionToSet, value);
+            else
+                optionDiscardedWarning();
+            resetOption();
+        }
+        
+        public void SetFloatValue(float value)
+        {
+            if(validToExecute(value.GetType(), optionsHelper.OptionType(_optionToSet)))
+                _settingsService.SetOption<float>(_optionToSet, value);
+            else
+                optionDiscardedWarning();
+            resetOption();
+        }
+        
+        public void SetBoolValue(bool value)
+        {
+            if(validToExecute(value.GetType(), optionsHelper.OptionType(_optionToSet)))
+                _settingsService.SetOption<bool>(_optionToSet, value);
+            else
+                optionDiscardedWarning();
+            resetOption();
+        }
+        
+        public void SetStringValue(string value)
+        {
+            if(validToExecute(value.GetType(), optionsHelper.OptionType(_optionToSet)))
+                _settingsService.SetOption<string>(_optionToSet, value);
+            else
+                optionDiscardedWarning();
+            resetOption();
+        }
+        
+        private bool validToExecute(System.Type optionType, System.Type valueType)
+        {
+            if(_optionsRegistered == 1 && Enum.IsDefined(typeof(AudioVisualOptions), _optionToSet) && 
+                optionType == valueType)
+                return true;
+            else
+                return false;
+        }
+        
+        private void resetOption()
+        {            
+            _optionToSet = (AudioVisualOptions)(-1);
+            _optionsRegistered = 0;            
+        }
+        
+        private void optionDiscardedWarning()
+        {
+            Debug.LogWarning("Settings execution was stopped, _optionsRegistered = " + _optionsRegistered + 
+                " _optionToSet: " + Enum.GetName(typeof(AudioVisualOptions), _optionToSet));
         }
     }
 }
--- Assets/Services/Settings/OptionsEnum.cs
+++ Assets/Services/Settings/OptionsEnum.cs
@@ -9,4 +9,29 @@ namespace GameSettings
         ResolutionScaling,
         Graphics
     }
+    
+    
+    public static class optionsHelper
+    {
+        public static System.Type OptionType(AudioVisualOptions option)
+        {
+            switch (option)
+            {
+                case AudioVisualOptions.MasterVolume:
+                    return typeof(float);
+                case AudioVisualOptions.MusicVolume:
+                    return typeof(float);
+                case AudioVisualOptions.SfxVolume:
+                    return typeof(float);
+                case AudioVisualOptions.Vibration:
+                    return typeof(bool);
+                case AudioVisualOptions.ResolutionScaling:
+                    return typeof(float);
+                case AudioVisualOptions.Graphics:
+                    return typeof(int);
+                default:
+                    return typeof(int);
+            }
+        }
+    }
 }
